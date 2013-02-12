b7054ae535639415681bfc430264cbc8b13f1415
Oleksandr Usov <a.s.usov@gmail.com>
2013-02-12 02:28:28 +0000

    filtering

--- main.py
+++ main.py
@@ -4,25 +4,51 @@ from PyQt4 import QtCore, QtGui
 from balancer import schema, db
 from balancer.window_ui import Ui_MainWindow
 
-class TransactionsViewModel(QtCore.QAbstractTableModel):
+class cacher(object):
+    def __init__(self):
+        super(cacher, self).__init__()
+        self.__cache = dict()
+
+    def get(self, typ, oid):
+        key = (typ, oid)
+        i = self.__cache.get(key)
+        if i is None:
+            i = self.db.query(typ).get(oid)
+            if i:
+                self.__cache[key] = i
+        return i
+
+# FIXME: creepy -- need to put my mixin BEFORE qt model
+class TransactionsViewModel(cacher, QtCore.QAbstractTableModel):
     def __init__(self, db):
         super(TransactionsViewModel, self).__init__()
 
         self.acc = None
+        self.payeeFilter = None
         self.db = db
         self.cols = ['trn_type', 'amount', 'payee', 'date', 'category_id']
         self.tids = []
-        self.trns = {}
 
-    def load_data(self, acc):
+    def set_account(self, acc):
         if self.acc == acc:
             return
-
         self.acc = acc
+        self.reload_data()
+
+    def set_filter(self, payeeFilter):
+        f = None if payeeFilter == '' else payeeFilter
+        if self.payeeFilter == f:
+            return
+        self.payeeFilter = f
+        self.reload_data()
+
+    def reload_data(self):
         tn = schema.Transaction
-        self.tids = self.db.query(tn.id).filter_by(account_id = acc)\
-                .order_by(tn.date.desc(), tn.payee, tn.amount).all()
-        self.trns = {}
+        q = self.db.query(tn.id).filter_by(account_id = self.acc)
+        if self.payeeFilter:
+            q = q.filter(tn.payee.like('%{}%'.format(self.payeeFilter)))
+
+        self.tids = q.order_by(tn.date.desc(), tn.payee, tn.amount).all()
         self.modelReset.emit()
 
     def rowCount(self, parent):
@@ -34,9 +60,7 @@ class TransactionsViewModel(QtCore.QAbstractTableModel):
     def data(self, idx, role):
         if not idx.isValid() or role != QtCore.Qt.DisplayRole:
             return QtCore.QVariant()
-        if not self.trns.has_key(idx.row()):
-            self.trns[idx.row()] = self.db.query(schema.Transaction).get(self.tids[idx.row()])
-        t = self.trns[idx.row()]
+        t = self.get(schema.Transaction, self.tids[idx.row()])
         return QtCore.QVariant(unicode(getattr(t, self.cols[idx.column()])))
 
     def headerData(self, sect, orient, role):
@@ -86,28 +110,31 @@ class Main(QtGui.QMainWindow):
         self.ui.transactionsList.clear()
         self.ui.transactionsList.addTopLevelItems(items)
 
-    def get_data(self, acc):
-        tn = schema.Transaction
-        bn = schema.Balance
-        tns = self.db.query(tn).filter_by(account_id = acc)\
-                .order_by(tn.date.desc(), tn.payee, tn.amount).all()
-        bns = self.db.query(bn).filter_by(account_id = acc)\
-                .order_by(bn.date.desc()).all()
-        while len(tns) != 0 or len(bns) != 0:
-            if len(tns) == 0:
-                yield bns.pop(0)
-            elif len(bns) == 0:
-                yield tns.pop(0)
-            elif bns[0].date >= tns[0].date:
-                yield bns.pop(0)
-            else:
-                yield tns.pop(0)
+## FIXME: mix transactions and balances
+    #def get_data(self, acc):
+        #tn = schema.Transaction
+        #bn = schema.Balance
+        #tns = self.db.query(tn).filter_by(account_id = acc)\
+                #.order_by(tn.date.desc(), tn.payee, tn.amount).all()
+        #bns = self.db.query(bn).filter_by(account_id = acc)\
+                #.order_by(bn.date.desc()).all()
+        #while len(tns) != 0 or len(bns) != 0:
+            #if len(tns) == 0:
+                #yield bns.pop(0)
+            #elif len(bns) == 0:
+                #yield tns.pop(0)
+            #elif bns[0].date >= tns[0].date:
+                #yield bns.pop(0)
+            #else:
+                #yield tns.pop(0)
 
     def on_accountsList_currentItemChanged(self, current=None, prev=None):
         if current is None:
             return
-        #self.show_transactions(current.db_item.id)
-        self.model.load_data(current.db_item.id)
+        self.model.set_account(current.db_item.id)
+
+    def on_payeeFilter_editingFinished(self):
+        self.model.set_filter(str(self.ui.payeeFilter.text()))
 
 if __name__ == '__main__':
     import sys
